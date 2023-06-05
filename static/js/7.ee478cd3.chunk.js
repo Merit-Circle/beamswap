532b83624a4911e12347c920a808520007244fe5
Martin Kainzbauer <m.kainzbauer@xtools.at>
2023-06-05 22:45:46 +0200

    initial build

---
+++ static/js/7.ee478cd3.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonp@beamswap/ui"]=this["webpackJsonp@beamswap/ui"]||[]).push([[7],{743:function(n,i){},754:function(n,i){},756:function(n,i){}}]);
+//# sourceMappingURL=7.ee478cd3.chunk.js.map
---
+++ static/js/8.725cf0c7.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonp@beamswap/ui"]=this["webpackJsonp@beamswap/ui"]||[]).push([[8],{709:function(p,s){}}]);
+//# sourceMappingURL=8.725cf0c7.chunk.js.map
